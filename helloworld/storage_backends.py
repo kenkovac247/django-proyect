f3227137c1d108cba9d33314941ccdacb18a9074
Kennelth Kovac <kennelth.kovac@escala24x7.com>
2025-09-25 18:56:41 -0400

    ...

--- helloworld/storage_backends.py
+++ helloworld/storage_backends.py
@@ -2,5 +2,10 @@ import os
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = os.environ.get('AWS_BUCKET_MEDIA')
+    bucket_name = os.environ.get('AWS_BUCKET_MEDIA', 'tu-proyecto-media')
     location = 'media'
+    file_overwrite = False
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['bucket_name'] = self.bucket_name
+        super().__init__(*args, **kwargs)
